5feb3b7d4328b07ac1e2fc483c70d317fa8a6cc7
tsovke <tsovke@gmail.com>
2024-09-09 17:28:24 +0800

    update

---
+++ code/buffer/buffer.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <atomic>
+#include <cassert>
+#include <cstddef>
+#include <cstring>
+#include <iostream>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <vector>
+
+class Buffer {
+public:
+  Buffer(int initBuffSize = 1024);
+  ~Buffer() = default;
+  size_t WritableBytes() const;
+  size_t ReadableBytes() const;
+  size_t PrependableBytes() const;
+
+  const char *Peek() const;
+  void EnsureWritable(size_t len);
+  void HasWritten(size_t len);
+
+  void Retrieve(size_t len);
+  void RetrieveUntil(const char *end);
+  void RetrieveAll();
+  std::string RetrieveAllToStr();
+
+  const char *BeginWriteConst() const;
+  char *BeginWrite();
+
+  void Append(const char *str, size_t len);
+  void Append(const std::string &str);
+  void Append(const void *data, size_t len);
+  void Append(const Buffer &buff);
+
+  ssize_t ReadFd(int fd, int *Errno);
+  ssize_t WriteFd(int fd, int *Errno);
+
+private:
+  char *BeginPtr_();
+  const char *BeginPtr_() const;
+  void MakeSpace_(size_t len);
+
+  std::vector<char> buffer_;
+  std::atomic<std::size_t> readPos;
+  std::atomic<std::size_t> writePos;
+};
---
+++ code/http/httpresponse.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <unordered_map>
+
+#include "../buffer/buffer.h"
+#include "../log/log.h"
+
+class HttpResponse {
+public:
+private:
+};
